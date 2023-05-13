11be6a93efe842355b85dd8c74d57011db5fa046
Zeyad Khaled Sultan <ziadkhaled2260.zk@gmail.com>
2023-05-14 02:14:15 +0300

    Chapter 3: FindingWebElements

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,6 +1,8 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTests {
@@ -12,9 +14,11 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/"); // getting the link we will work on
         driver.manage().window().maximize(); // maximize the window
-        //driver.manage().window().fullscreen(); // setting it to full screen
-        //driver.manage().window().setSize(new Dimension(375, 812)); // setting size
-        System.out.println(driver.getTitle()); // getting the page title
+        WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content")); // getting the element
+        shiftingContent.click(); // clicking on it
+        WebElement ex1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        ex1.click();
+        System.out.println(driver.findElements(By.tagName("li")).size()); // getting the size of the list
         driver.quit(); // closing the session of the whole browser
     }
 
