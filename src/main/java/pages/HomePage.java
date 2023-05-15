4ed76a146a910b30bd750a78b4224e7574aee7a7
Zeyad Khaled Sultan <ziadkhaled2260.zk@gmail.com>
2023-05-15 19:54:15 +0300

    Chapter 9: Wait Strategies

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -46,6 +46,11 @@ public class HomePage {
         return new WYSIWYGEditorPage(driver);
     }
 
+    public DynamicLoadingPage clickDynamicLoading() {
+        clickLink("Dynamic Loading");
+        return new DynamicLoadingPage(driver);
+    }
+
     public void clickLink(String linkText) {
         driver.findElement(By.linkText(linkText)).click();;
     }
